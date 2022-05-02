a206dde13c090e9c1572743bc34656982359e472
RiccardoTancredi <ric.tancredi@gmail.com>
2022-05-02 21:55:50 +0200

    Introducing Hopping analysis

--- file_hopping.py
+++ file_hopping.py
@@ -4,60 +4,84 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 class Txt_hop:
-    def __init__(self, dir_name, number) -> None:
+    def __init__(self, dir_name, number, first_time_molecule=False) -> None:
         self.dir_name = dir_name
         self.number = str(number)
         self.name = self.dir_name + '/' + self.number + 'hop'
-        self.first_time_molecule = False # if the file is open for the first time use method _removeBegin
+        self.first_time_molecule = first_time_molecule # if the file is open for the first time use method _removeBegin
 
     def _removeBegin(self):
-        if self.first_time_molecule:
-            a_file = open(self.name+'A.txt', "r")
-            lines = a_file.readlines()
-            a_file.close()
-            if "#Begin Write:" in lines[0]:
-                del lines[0]
-            new_file = open(self.name+'A.txt', "w+")
-            for line in lines:
-                new_file.write(line)
-            new_file.close()
+        a_file = open(self.name+'A.txt', "r")
+        lines = a_file.readlines()
+        a_file.close()
+        if "#Begin Write:" in lines[0]:
+            del lines[0]
+        new_file = open(self.name+'A.txt', "w+")
+        for line in lines:
+            new_file.write(line)
+        new_file.close()
 
     def bricolage(self):
-        self._removeBegin()
-        alphabet = ['A','B','C','D','E','F','G','H','I','L','M','N','O','P','Q','R','S','T','U','V','Z','J','K','X','Y','W'] 
-        data = []
-        # Here I check if there are files for the same molecul and I open them and put them into the same dataFrame
-        for i in alphabet:
-            moment_name = str(self.name+i+'.txt')
-            if os.path.isfile(moment_name):
-                data.append(pd.read_csv(moment_name, sep = "\t"))
-            else:
-                break
-        data_frame = pd.concat([j for j in data], ignore_index=True) # I add together all the datasets
-        data_frame.rename(columns = {'#CycleCount/n':'CycleCount'}, inplace = True) 
-        # Remove the keywords: #EndWrite and #Skipped
-        data_frame.drop(data_frame[data_frame['CycleCount'] == "#End Write"].index, inplace = True)
-        data_frame.drop(data_frame[data_frame['CycleCount'] == "#Skipped"].index, inplace = True)
-        data_frame['CycleCount'] = pd.to_numeric(data_frame['CycleCount'])
-        data_frame = self._freq(data_frame)
-        data_frame = data_frame.reset_index()
-        data_frame = data_frame.drop(['index'], axis=1)
-        data_frame = data_frame.drop(['level_0'], axis=1)
-        # Create the new column lamnda, variable useful later
-        data_frame['lambda'] = ((data_frame['A_dist-Y']+data_frame['B_dist-Y']).values)/2
+        if self.first_time_molecule:
+            self._removeBegin()
+            alphabet = ['A','B','C','D','E','F','G','H','I','L','M','N','O','P','Q','R','S','T','U','V','Z','J','K','X','Y','W'] 
+            data = []
+            # Here I check if there are files for the same molecul and I open them and put them into the same dataFrame
+            for i in alphabet:
+                moment_name = str(self.name+i+'.txt')
+                if os.path.isfile(moment_name):
+                    data.append(pd.read_csv(moment_name, sep = "\t"))
+                else:
+                    break
+            
+            data_frame = pd.concat([j for j in data], ignore_index=True) # I add together all the datasets
+            data_frame.rename(columns = {'#CycleCount/n':'CycleCount'}, inplace = True) 
+            # Remove the keywords: #EndWrite and #Skipped
+            data_frame.drop(data_frame[data_frame['CycleCount'] == "#End Write"].index, inplace = True)
+            data_frame.drop(data_frame[data_frame['CycleCount'] == "#Skipped"].index, inplace = True)
+            data_frame['CycleCount'] = pd.to_numeric(data_frame['CycleCount'])
+            data_frame = self._freq(data_frame)
+            data_frame = data_frame.reset_index()
+            data_frame = data_frame.drop(['index'], axis=1)
+            data_frame = data_frame.drop(['level_0'], axis=1)
+            # Create the new column lamnda, variable useful later
+            data_frame['lambda'] = ((data_frame['A_dist-Y']+data_frame['B_dist-Y']).values)/2
 
-        '''
-            Store this dataFrame into a .txt file
-        '''
-        data_frame['Y_force'] = data_frame['Y_force'].abs()
-        if not os.path.isfile(self.name+'.txt'):
-            with open(os.path.join(self.dir_name, self.number+'.txt'),'w') as outfile:
-                data_frame[['X_force', 'Z_force', 'lambda', 'Y_force', 'time(sec)']].to_string(outfile, index=False)
-        
-        return data_frame
+            '''
+                Store this dataFrame into a .txt file
+            '''
+            data_frame['Y_force'] = data_frame['Y_force'].abs()
+            self.dataframe = data_frame[['X_force', 'Z_force', 'lambda', 'Y_force', 'time(sec)']].copy()
+            if not os.path.isfile(self.name+'.txt'):
+                with open(os.path.join(self.dir_name, self.number+'hop'+'.txt'),'w') as outfile:
+                    self.dataframe.to_string(outfile, index=False)
+            '''
+                Store in sub files in order to better analyse the single hopping
+            '''
+            if not os.path.isfile(self.name+'_1'+'.txt'):
+                lista = self._jump()
+                for i in range(len(lista)-1):
+                    df2 = pd.DataFrame(columns = self.dataframe.columns)
+                    df2 = df2.append(self.dataframe.loc[lista[i]:lista[i+1]-1], ignore_index=True)
+                    if not os.path.isfile(self.name+'_'+str(i+1)+'.txt'):
+                        with open(os.path.join(self.dir_name, self.number+'_'+str(i+1)+'.txt'),'w') as outfile:
+                            df2.to_string(outfile, index=False)
+        else:
+            print("ciao sono qui")
+            data = []
+            data.append(pd.read_fwf(self.name+'.txt', colspecs = [(0, 9), (9, 17), (17, 28), (28, 37), (37, -1)]))
+            self.dataframe = pd.concat([j for j in data], ignore_index=True) # I add together all the datasets
+
+        return self.dataframe
 
     def _freq(self, data_frame):
         # here we want to take only rows with f = 100 kHz
         data_frame = data_frame[data_frame['CycleCount'].diff() == 1]
         data_frame = data_frame.reset_index()
         return data_frame
+
+    def _jump(self):
+        lista = self.dataframe.index[self.dataframe['time(sec)'].diff() >= 1].tolist()
+        lista.insert(0, 0) # to cut correctly the dataframe into files
+        lista.append(self.dataframe.shape[0])
+        return lista
--- graph_hop.py
+++ graph_hop.py
@@ -8,30 +8,40 @@ from scipy.odr import *
 import math
 
 class Graph_hop:
-    def __init__(self, dir_name, number) -> None:
+    def __init__(self, dir_name, number, number_file) -> None:
         self.dir_name = dir_name
         self.number = str(number)
-        self.name = self.dir_name + '/' + self.number + '.txt'
+        self.number_file = str(number_file)
+        self.name = self.dir_name + '/' + self.number + '_' + self.number_file 
         
-    def do_graph(self):
+    def do_graph(self, time_range):
         data = []
-        data.append(pd.read_fwf(self.name, colspecs = [(0, 9), (9, 17), (17, 28), (28, 37), (37, -1)]))
+        data.append(pd.read_fwf(self.name+'.txt', colspecs = [(0, 9), (9, 17), (17, 28), (28, 37), (37, -1)]))
         self.data_frame = pd.concat([j for j in data], ignore_index=True) # I add together all the datasets
         '''
             I would like to ask the teacher how to implement this thing
         '''
         # self._rebin()
-        
-        # print(self.data_frame[['lambda', 'Y_force']])
+
         # self.data_frame.plot.line(x ='lambda', y='Y_force')
-        plt.scatter(self.data_frame['time(sec)'], self.data_frame['Y_force'], label='Y_force')
+        if time_range:
+            lista = []
+            lista.append(self.data_frame.index[self.data_frame['time(sec)'] == time_range[0]].tolist()[0])
+            lista.append(self.data_frame.index[self.data_frame['time(sec)'] == time_range[1]].tolist()[0])
+            self.data_frame = self.data_frame.loc[lista[0]:lista[1]-1]
+            self.data_frame = self.data_frame.reset_index()
+            self.data_frame = self.data_frame.drop(['index'], axis=1)
+       
+        df_mean = self.data_frame['Y_force'].mean()
+        df_std = self.data_frame['Y_force'].std()
+        plt.plot(self.data_frame['time(sec)'], self.data_frame['Y_force'], label='Y_force')
+        plt.axhline(y = df_mean, color = 'b', linestyle = 'dashed', label = '$\mu$')    
+        plt.axhline(y = df_mean+3*df_std, color = 'r', linestyle = 'dashed', label = '$\mu\pm3\sigma$')   
+        plt.axhline(y = df_mean-3*df_std, color = 'r', linestyle = 'dashed')   
         plt.ylabel('$f_y$(pN)')
         plt.xlabel('$t$(s)')
         plt.title(self.name)
-        # f_F, f_U = self.maximum_f(False)
-        # if f_F and f_U:
-        #     plt.annotate(f'f_F', xy=(self.data_frame['lambda'][np.where(self.data_frame['Y_force'].values == f_F)[0][0]], f_F),) 
-        #     plt.annotate(f'f_U', xy=(self.data_frame['lambda'][np.where(self.data_frame['Y_force'].values == f_U)[0][0]], f_U),) 
         plt.legend()
         plt.show()
-        # return [f_F, f_U]
+
+        return self.data_frame
